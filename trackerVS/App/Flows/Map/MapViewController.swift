a0ad9c9d860659dba6ccab8eae47a33804cd5f23
Vladimir Sirel <exit551@ya.ru>
2022-04-24 20:52:08 +0300

    Base config maps view

--- trackerVS/App/Flows/Map/MapViewController.swift
+++ trackerVS/App/Flows/Map/MapViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import GoogleMaps
+import PinLayout
 
 class MapViewController: UIViewController {
-
+    // MARK: - Properties
+    private var mapHolderView = GMSMapView()
+    let baseCoordinates = CLLocationCoordinate2D(latitude: 58.52107786398308, longitude: 31.275274938749853)
+    var baseZoom: Float = 15.0
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureMap()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        mapHolderView.pin.all()
+    }
+    
+    // MARK: - Methods
+    func configureMap() {
+        view.addSubview(mapHolderView)
+        
+        let camera = GMSCameraPosition(target: baseCoordinates, zoom: baseZoom)
+        mapHolderView.camera = camera
+        mapHolderView.mapType = .normal
+    }
+    
 }
-
