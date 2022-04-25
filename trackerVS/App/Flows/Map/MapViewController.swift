d18870e8fb3125af2b0c177534f88989a54388fd
Vladimir Sirel <exit551@ya.ru>
2022-04-25 23:22:02 +0300

    Complite hw tasks 5-8

--- trackerVS/App/Flows/Map/MapViewController.swift
+++ trackerVS/App/Flows/Map/MapViewController.swift
@@ -15,21 +15,68 @@ class MapViewController: UIViewController {
     private let baseCoordinates = CLLocationCoordinate2D(latitude: 58.52107786398308, longitude: 31.275274938749853)
     private let baseZoom: Float = 15.0
     
+    private var marker: GMSMarker?
+    private var route: GMSPolyline? {
+        didSet {
+            route?.strokeColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            route?.strokeWidth = 3
+        }
+    }
+    private var routePath: GMSMutablePath?
     private var locationManager: CLLocationManager?
+    
+    private var buttonCurrentLocation: UIButton = {
+        let button = UIButton(type: .system)
+        button.titleLabel?.font = .systemFont(ofSize: 15.0)
+        button.titleLabel?.tintColor = .black
+        button.backgroundColor = .orange
+        button.setTitle("Get my location", for: .normal)
+        button.layer.cornerRadius = 5.0
+        button.clipsToBounds = true
+        button.addTarget(self, action:#selector(buttonCurrentLocationTap), for: .touchUpInside)
+        return button
+      }()
+    private var buttonStopUpdatingLocation: UIButton = {
+        let button = UIButton(type: .system)
+        button.titleLabel?.font = .systemFont(ofSize: 15.0)
+        button.titleLabel?.tintColor = .white
+        button.backgroundColor = .brown
+        button.setTitle("Stop updating location", for: .normal)
+        button.layer.cornerRadius = 5.0
+        button.clipsToBounds = true
+        button.addTarget(self, action:#selector(buttonStopUpdatingLocationTap), for: .touchUpInside)
+        return button
+      }()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureMap()
+        configureUI()
         configurLocationManager()
+        configureRoutePath()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         mapHolderView.pin.all()
+        buttonCurrentLocation.pin.bottom(30.0).right(20.0).height(30.0).minWidth(160.0).sizeToFit()
+        buttonStopUpdatingLocation.pin.bottom(30.0).left(20.0).height(30.0).minWidth(160.0).sizeToFit()
     }
     
-    // MARK: - Methods
+    // MARK: - Configure
+    private func configureUI() {
+        mapHolderView.addSubview(buttonCurrentLocation)
+        mapHolderView.addSubview(buttonStopUpdatingLocation)
+    }
+    
+    private func configureRoutePath() {
+        route?.map = nil
+        route = GMSPolyline()
+        routePath = GMSMutablePath()
+        route?.map = mapHolderView
+    }
+    
     private func configureMap() {
         view.addSubview(mapHolderView)
         
@@ -38,8 +85,21 @@ class MapViewController: UIViewController {
         mapHolderView.mapType = .normal
     }
     
+    // MARK: - Methods
+    private func addMarker(_ coordinate: CLLocationCoordinate2D) {
+        marker = GMSMarker(position: coordinate)
+        marker?.map = mapHolderView
+    }
     
+    // MARK: - Button actions
+    @objc private func buttonCurrentLocationTap(sender : UIButton) {
+        configureRoutePath()
+        locationManager?.startUpdatingLocation()
+    }
     
+    @objc private func buttonStopUpdatingLocationTap(sender : UIButton) {
+        locationManager?.stopUpdatingLocation()
+    }
 }
 
 // MARK: - CLLocationManagerDelegate
@@ -47,8 +107,31 @@ extension MapViewController: CLLocationManagerDelegate {
     private func configurLocationManager() {
         if CLLocationManager.locationServicesEnabled() {
             locationManager = CLLocationManager()
-            locationManager?.requestWhenInUseAuthorization()
             locationManager?.delegate = self
+            locationManager?.requestWhenInUseAuthorization()
+            locationManager?.allowsBackgroundLocationUpdates = true
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+         print("error:: \(error.localizedDescription)")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager?.requestLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        routePath?.add(location.coordinate)
+        route?.path = routePath
+
+        let position = GMSCameraPosition.camera(withTarget: location.coordinate , zoom: 15)
+        mapHolderView.animate(to: position)
+        print("location:: \(location.coordinate)")
+        
+        addMarker(location.coordinate)
     }
 }
