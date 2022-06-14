ac7c8ada6ad0ea844bed8c44236edde04f419c76
Vladimir Sirel <exit551@ya.ru>
2022-06-14 21:41:31 +0300

    Added UIImagePickerController

--- trackerVS/App/Flows/Avatar/AvatarViewController.swift
+++ trackerVS/App/Flows/Avatar/AvatarViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class AvatarViewController: UIViewController, Coordinating {
+final class AvatarViewController: UIViewController, Coordinating, UINavigationControllerDelegate {
     var coordinator: Coordinator?    
     
     @IBOutlet weak var avatarImageView: UIImageView!
@@ -16,7 +16,27 @@ final class AvatarViewController: UIViewController, Coordinating {
         super.viewDidLoad()
     }
 
-    @IBAction func tapCameraButton(_ sender: Any) {
-        
+    @IBAction func tapGetPhotoButton(_ sender: Any) {
+        configImagePicker(source: .photoLibrary)
+    }
+    
+    func configImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+}
+
+extension AvatarViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        avatarImageView.image = info[.editedImage] as? UIImage
+        avatarImageView.contentMode = .scaleToFill
+        avatarImageView.clipsToBounds = true
+        dismiss(animated: true)
     }
 }
